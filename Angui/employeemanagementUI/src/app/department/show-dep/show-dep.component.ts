06ba605d113aebaa8dcf3a481164d435b6900a77
sajjadm624 <sajjadm624@gmail.com>
2021-01-24 01:48:18 +0600

    Employee App Deploy

---
+++ Angui/employeemanagementUI/src/app/department/show-dep/show-dep.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import{SharedService} from 'src/app/shared.service'
+
+@Component({
+  selector: 'app-show-dep',
+  templateUrl: './show-dep.component.html',
+  styleUrls: ['./show-dep.component.css']
+})
+export class ShowDepComponent implements OnInit {
+
+  constructor(private service:SharedService) { }
+
+  DepartmentList:any=[]
+
+  ngOnInit(): void {
+    this.refreshDepList();
+  }
+
+  refreshDepList(){
+  this.service.getDepList().subscribe(data=>{
+    this.DepartmentList=data;
+   });
+  }
+
+}
